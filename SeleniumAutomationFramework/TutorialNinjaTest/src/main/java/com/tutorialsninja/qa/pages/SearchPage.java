ffb26200992d55786afdd4f65fb6dd20852ee995
Najam <najam.nxb@gmail.com>
2024-01-17 13:03:11 +0500

    More Details are added

--- SeleniumAutomationFramework/TutorialNinjaTest/src/main/java/com/tutorialsninja/qa/pages/SearchPage.java
+++ SeleniumAutomationFramework/TutorialNinjaTest/src/main/java/com/tutorialsninja/qa/pages/SearchPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-//Updated Comment
+//Updated Comment --- Added more details
 
 public class SearchPage {
 	
